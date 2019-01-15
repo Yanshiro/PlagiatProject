5db870b9b7cff2876c012a1f054c5a4a9a7a6561
Yannis <38785824+Yanshiro@users.noreply.github.com>
2019-01-15 23:39:47 +0100

    update php et html
    update php et html

--- upload.php
+++ upload.php
@@ -1,6 +1,18 @@
-<?php
-if (isset($_FILES["file1"]["name"])) {
-    echo = $_FILES["file1"]["name"];
-//echo = $_FILES["file2"]["name"];
+<?php 
+ if(isset($_POST['submit'])){
+    $name       = $_FILES['file']['name'];  
+    $temp_name  = $_FILES['file']['tmp_name'];  
+    $name2       = $_FILES['file2']['name'];  
+    $temp_name2  = $_FILES['file2']['tmp_name'];  
+    if(isset($name) && isset($name2)){
+        if(!empty($name) && !empty($name2)){ 
+            $location = 'uploadfiles/';      
+            if(move_uploaded_file($temp_name, $location.$name) && move_uploaded_file($temp_name2, $location.$name2)){
+                echo 'File uploaded successfully';
+            }
+        }       
+    }  else {
+        echo 'You should select a file to upload !!';
+    }
 }
-
+?>
